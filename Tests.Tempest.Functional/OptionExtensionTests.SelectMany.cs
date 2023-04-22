88b4a73e376b69ef54598f04db645a576bbf41fb
Sean <seanbatten58@gmail.com>
2023-04-22 09:39:08 +0100

    Naming

--- Tests.Tempest.Functional/OptionExtensionTests.SelectMany.cs
+++ Tests.Tempest.Functional/OptionExtensionTests.SelectMany.cs
@@ -14,7 +14,7 @@ namespace Tests.Tempest.Functional
     public partial class OptionExtensionTests
     {
         [Test]
-        public void SelectMany_Linq()
+        public void SelectMany_Linq_1()
         {
             Option<int> x = new(10);
             Option<int> y = new(20);
@@ -27,7 +27,22 @@ namespace Tests.Tempest.Functional
         }
 
         [Test]
-        public void SelectMany_Linq_None()
+        public void SelectMany_Linq_2()
+        {
+            Option<int> x = new(10);
+            Option<int> y = new(20);
+            Option<int> z = new(30);
+
+            var total = from a in x
+                        from b in y
+                        from c in z
+                        select a + b + c;
+
+            Assert.That(total.Value(), Is.EqualTo(60));
+        }
+
+        [Test]
+        public void SelectMany_Linq_None_1()
         {
             Option<int> x = new(10);
             Option<int> y = Option.None;
@@ -35,6 +50,21 @@ namespace Tests.Tempest.Functional
             var total = from a in x
                         from b in y
                         select a + b;
+
+            Assert.That(total, Is.EqualTo(Option.None));
+        }
+
+        [Test]
+        public void SelectMany_None_2()
+        {
+            Option<int> x = new(10);
+            Option<int> y = new(20);
+            Option<int> z = Option.None;
+
+            var total = from a in x
+                        from b in y
+                        from c in z
+                        select a + b + c;
 
             Assert.That(total, Is.EqualTo(Option.None));
         }
